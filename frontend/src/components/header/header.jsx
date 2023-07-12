eba15cadc0846898457c2dd4c0af1a2de18beacf
anirudhg001 <annigupta007@gmail.com>
2023-07-12 20:37:49 +0530

    navigation bar changes

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,6 +1,8 @@
 import {useEffect, useRef} from "react";
 import logo from '../../assets/images/logo.png'
+import userImg from '../../assets/images/avatar-icon.png'
 import {NavLink, Link} from 'react-router-dom'
+import {BiMenu} from 'react-icons/bi'
 
 const navLinks= [
   {
@@ -24,6 +26,16 @@ const navLinks= [
 ]
 
 const Header = () => {
+
+const headerRef = useRef(null)
+const menuRef= useRef(null)
+
+const handleStickyHeader =()=>{
+  window.addEventListener('scroll', ()=>{
+    if(document.body.scrollTop >)
+  })
+}
+
   return <header className="header flex items-center">
     <div className="container">
       <div className="flex items-center justify-between">
@@ -41,13 +53,37 @@ const Header = () => {
   navLinks.map((link, index)=><li key={index}>
     <NavLink to={link.path} className={navClass=> navClass.isActive ? 
       'text-primaryColor text-[16px] leading-7 font-[600]' :
-       'text-textColor text-[16px] leading-7 font-[500]'}>{link.display}
+       'text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor'
+       }>{link.display}
 
     </NavLink>
   </li>)
 }
   </ul>
 </div>
+
+           {/*======= nav right ========*/}
+
+<div className="flex items-center gap-4">
+  <div className="hidden">
+    <Link to ='/'>
+      <figure className="w-[35px] h-[35px] rounded-full">
+        <img src={userImg} className="w-full rounded-full" alt="" />
+      </figure>
+    </Link>
+  </div>
+
+
+<Link to='/login'>
+  <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">Login</button>
+</Link>
+
+<span className="md:hidden">
+  <BiMenu className='w-6 h-6 cursor-pointer'/>
+</span>
+
+</div>
+
       </div>
     </div>
   </header>
