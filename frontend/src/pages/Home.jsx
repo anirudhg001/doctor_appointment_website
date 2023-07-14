c4169917fd98b13be29d30857789600c9c04e57b
anirudhg001 <annigupta007@gmail.com>
2023-07-14 21:41:52 +0530

    about page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -159,6 +159,18 @@ const Home = () => {
   </div>
 </section>
   <About />
+
+  {/*============= services section ============ */}
+  <section>
+    <div className="container">
+      <div className="xl:w-[470px] mx-auto">
+        <h2 className="heading text-center">Our medical services</h2>
+        <p className="text__para text-center">World class care for everyone. Our health system offers unmatched, 
+        expert health care.</p>
+      </div>
+    </div>
+  </section>
+  {/*============= services section end ============ */}
   </>
 };
 
