f861a89b7fde81aaab8e6ba813d39cc7f984e32d
anirudhg001 <annigupta007@gmail.com>
2024-01-28 04:24:47 +0530

    import classes and build home.jsx

---
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import patientavatar from "../../assets/images/patient-avatar.png";
+
+const Testimonial = () => {
+  return <div>Testimonial</div>;
+};
+
+export default Testimonial;
